d49031c32a209b900500e166e054bbf91eea470f
AbdeLmonaim1 <abdelmonaimahdoud3@gmail.com>
2024-12-17 22:22:18 +0100

    Add Class Animation, Audio, Initialisation and Images, Sounds interfaces

---
+++ src/main/java/com/example/utils/Animation.java
@@ -0,0 +1,22 @@
+package com.example.utils;
+
+import javafx.animation.FadeTransition;
+import javafx.animation.Interpolator;
+import javafx.animation.TranslateTransition;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
+
+public class Animation {
+    public static void animateLogoSpaceInvaders(ImageView imgLogo, double fromY, double toY, int delay, double fromValue, double toValue, int fadeDuration ) {
+        TranslateTransition animation = new TranslateTransition(Duration.millis(delay), imgLogo);
+        animation.setFromY(fromY);
+        animation.setToY(toY);
+        animation.setInterpolator(Interpolator.EASE_OUT);
+        animation.play();
+
+        FadeTransition fade = new FadeTransition(Duration.millis(fadeDuration), imgLogo);
+        fade.setFromValue(fromValue);
+        fade.setToValue(toValue);
+        fade.play();
+    }
+}
