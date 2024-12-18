a89a8560e860aad7775b72ea50f450a73070dca9
amine-ghazoui <ghazouimohamedamine@gmail.com>
2024-12-18 19:15:07 +0100

    Add utility methods for alien movement and image display

--- src/main/java/com/example/utils/Images.java
+++ src/main/java/com/example/utils/Images.java
@@ -1,5 +1,20 @@
 package com.example.utils;
 
+import javafx.scene.image.Image;
+
 public interface Images {
+
+    String PATH = "FILE:./src/main/resources/com/example/images/";
+
+    /***********************************  ALIEN  ***************************************/
+
+    Image ALIEN_HIGH_1 = new Image(PATH + "alienHigh1.png");
+    Image ALIEN_HIGH_2 = new Image(PATH + "alienHigh2.png");
+    Image ALIEN_MIDDLE_1 = new Image(PATH + "alienMiddle1.png");
+    Image ALIEN_MIDDLE_2 = new Image(PATH + "alienMiddle2.png");
+    Image ALIEN_BOTTOM_1 = new Image(PATH + "alienBottom1.png");
+    Image ALIEN_BOTTOM_2 = new Image(PATH + "alienBottom2.png");
+
+
 
 }
