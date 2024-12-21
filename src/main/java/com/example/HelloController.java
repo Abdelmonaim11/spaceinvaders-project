608d962fc27196906bd18b96b036da9ea6a53fec
amine-ghazoui <ghazouimohamedamine@gmail.com>
2024-12-21 16:06:18 +0100

    add interface

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -1,14 +1,80 @@
 package com.example;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Pane;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Pane board;
+
+    @FXML
+    private ImageView heart1;
+
+    @FXML
+    private ImageView heart2;
+
+    @FXML
+    private ImageView heart3;
+
+    @FXML
+    private ImageView heart4;
+
+    @FXML
+    private ImageView heart5;
+
+    @FXML
+    private ImageView imgLogo;
+
+    @FXML
+    private Label lblEndGame;
+
+    @FXML
+    private Label lblFPS;
+
+    @FXML
+    private Label lblLeftScore;
+
+    @FXML
+    private Label lblRightScore;
+
+    @FXML
+    private TextField nameShip;
+
+    @FXML
+    void addShip(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onKeyPressed(KeyEvent event) {
+
+    }
+
+    @FXML
+    void onKeyReleased(KeyEvent event) {
+
+    }
+
+    @FXML
+    void onStartAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onStopAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showShip(ActionEvent event) {
+
     }
 }
--- src/main/java/com/example/entities/Ship.java
+++ src/main/java/com/example/entities/Ship.java
@@ -14,5 +14,5 @@ public class Ship extends Entity{
         this.shipIsShooting = false;
     }
 
-    
+
 }
