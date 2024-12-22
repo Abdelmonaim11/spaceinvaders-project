477002e041a6c7234c57ca746943563ca94bd62d
AbdeLmonaim1 <abdelmonaimahdoud3@gmail.com>
2024-12-22 14:15:44 +0100

    Fixe module-info.java errors and run the application

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,16 +1,12 @@
-module com.example.spaceinvaders {
+module com.example {
+
+
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.web;
+    requires javafx.graphics;
+    requires javafx.media;
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-    requires com.almasb.fxgl.all;
+    opens com.example.controller to javafx.fxml;
 
-    opens com.example to javafx.fxml;
     exports com.example;
 }
